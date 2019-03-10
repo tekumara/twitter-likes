b4eb4e71b3b1ed215e45ddfde6b3078a2c512464
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2019-03-10 14:17:26 +1100

    remove complexity around fetching only unseen statuses/tweets
    it wasn't doing it anyway, and even if it was, it may cause some likes to be missed

--- README.md
+++ README.md
@@ -12,5 +12,3 @@ Download your twitter likes using an enhanced version of https://gist.github.com
 `make fetch` downloads favs to favs.db and favs.ndjson  
 `make dump` extract favs from favs.db to stdout  
 `make` to show all options
-
-NB: because of the way the timestamps work, every now and then you'll probably want to re-download everything. See comments in [get_favs.py](get_favs.py)
--- get_favs.py
+++ get_favs.py
@@ -4,19 +4,17 @@ Copyright 2015 Michael F. Lamb <http://datagrok.org>
 License: AGPLv3+
 
 ugly one-off script that uses tweepy to dump all favorites (aka "likes")
-into a dbm database file, because the twitter-provided data export does
+into a dbm database file and json text file, because the twitter-provided data export does
 not include them, because their users are not their customers, instead
 they are the product they sell to advertisers.
 
 re-running it will update the existing database file.
 
-warning warning: this script uses the since_id and max_id parameters in
-an attempt to retrieve only favs unseen since the last run. but the
+warning warning: this script doesn't use since_id and max_id parameters in
+an attempt to retrieve only favs unseen since the last run, because
 sequential ids are not assigned to favs, they're assigned to the
 original statuses. this means that if you fav a status that is older
-than your most recently fav'd status, we won't notice it! this means
-that you must periodically run a full re-download of all your favs to
-ensure none are missed.  🔥 this is fine.🔥
+than your most recently fav'd status, we won't notice it!
 
 warning: the twitter api docs caution that "there are limits to the
 number of tweets which can be accessed through the API." sooo if you're
@@ -58,32 +56,12 @@ def get_api():
     return tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 
-def get_unseen_statuses(api, min_id, max_id):
-    if min_id and max_id:
-        yield from tweepy.Cursor(api.favorites, creds.username,
-                                 count=200, since_id=max_id, include_entities=True, tweet_mode='extended').items()
-        yield from tweepy.Cursor(api.favorites, creds.username,
-                                 count=200, max_id=min_id, include_entities=True, tweet_mode='extended').items()
-    else:
-        yield from tweepy.Cursor(api.favorites, creds.username,
-                                 count=200, include_entities=True, tweet_mode='extended').items()
-
-
 def main():
     api = get_api()
     count = 0
     with dbm.open('favs.db', 'c') as db, open('favs.ndjson', 'at', buffering=1) as jsonfile:
-        try:
-            max_id = int(max(db.keys()))
-            # contrary to docs, twitter finds less than OR EQUAL TO
-            # max_id :(
-            min_id = int(min(db.keys())) - 1
-        except ValueError:
-            min_id = None
-            max_id = None
-        print("Looking for favs newer than %s, and also those older than %s" %
-              (max_id, min_id))
-        for status in get_unseen_statuses(api, min_id, max_id):
+        for status in tweepy.Cursor(api.favorites, creds.username,
+                                    count=200, include_entities=True, tweet_mode='extended').items():
             count = count + 1
             print(count)
             status_id = str(status.id)
